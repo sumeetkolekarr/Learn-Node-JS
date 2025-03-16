347365a4fbd9c073a94d43f708c36a0e936233ab
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-03-16 10:31:37 +0530

    Understood Querying the Data

--- Express JSON/index.js
+++ Express JSON/index.js
@@ -3,7 +3,53 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  // console.log(req.params);
+  res.send("Hello World");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // Can Only Send a response with 200 code and not multiple responses simultaneously
+    // res.status(200).send('No Products Matched Your Search')
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
